fd4347ed6cd5df58d07ac04b0d9b94d01ba4b449
Jakub <jakubglinka9@gmail.com>
2023-09-18 10:52:51 +0200

    Add evironmental variable SECRET_KEY

--- backend/investfree_server/investfree_server/settings.py
+++ backend/investfree_server/investfree_server/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2u(y9x^$+x1&fwby3-3f-s7&k8voh9dm560*b9-+n&_jy+b=%2'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
