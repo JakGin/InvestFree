04213cc5cce1d3c0af7983e5717ad62fa3654b6b
Jakub <jakubglinka9@gmail.com>
2023-09-19 20:31:31 +0200

    Setup a working fetch workflow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function handleClick() {
+    setCount(count => count + 1)
+  }
+
+  useEffect(() => {
+    async function getUsers() {
+      console.log(`${import.meta.env.VITE_API_URL}stocks`)
+      // const response = await fetch(`${import.meta.env.VITE_API_URL}stocks/`, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     "symbol": "AAPL",
+      //     "name": "Apple Inc",
+      //     "price": 100.00,
+      //   })
+      // })
+      const response = await fetch(`${import.meta.env.VITE_API_URL}stocks/`)
+      const data = await response.json()
+      console.log(data)
+      // const data = await response.json()
+      // console.log(data)
+      // console.log(await response.json())
+    }
+
+    getUsers()
+  }, [])
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is app.tsx</h1>
+      <h2>{count}</h2>
+      <button onClick={handleClick}>Click</button>
     </>
   )
 }
