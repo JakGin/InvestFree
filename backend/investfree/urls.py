a684db39f318c1086126a1f4dbaa20008f9e8f88
Jakub <jakubglinka9@gmail.com>
2023-09-21 18:21:57 +0200

    delete / from url ending

--- backend/investfree/urls.py
+++ backend/investfree/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     # Auth
-    path("api/register/", views.RegisterView.as_view(), name="register"),
-    path("api/login/", views.LoginView.as_view(), name="login"),
-    path("api/logout/", views.LogoutView.as_view(), name="logout"),
+    path("api/register", views.RegisterView.as_view(), name="register"),
+    path("api/login", views.LoginView.as_view(), name="login"),
+    path("api/logout", views.LogoutView.as_view(), name="logout"),
 
     # Main
-    path("api/users/", views.UsersView.as_view(), name="users"),
+    path("api/users", views.UsersView.as_view(), name="users"),
     path("api/user", views.UserView.as_view(), name="user"),
-    path("api/stocks/", views.StockView.as_view(), name="stocks")
+    path("api/stocks", views.StockView.as_view(), name="stocks")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
