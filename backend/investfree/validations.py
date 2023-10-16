3c3881d4f6c47aa0ca6089b6e64b48aad3a5e958
Jakub <jakubglinka9@gmail.com>
2023-10-16 12:53:03 +0200

    create register validator

--- backend/investfree/validations.py
+++ backend/investfree/validations.py
@@ -1,2 +1,26 @@
-def validate_user_data(data):
-    return data
+import re
+from django.core.validators import validate_email
+
+
+def validate_register_data(data):
+    if len(data) != 3:
+        raise Exception("Invalid request data")
+
+    try:
+        username = data["username"]
+        email = data["email"]
+        password = data["password"]
+    except KeyError:
+        raise Exception("Invalid request data")
+
+    if len(username) < 3:
+        raise Exception("Username must be at least 3 characters long")
+    
+    try:
+        validate_email(email)
+    except Exception:
+        raise Exception("Invalid email")
+    
+    password_pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+    if re.match(password_pattern, password) is None:
+        raise Exception("Password does not fulfil the requirements")
