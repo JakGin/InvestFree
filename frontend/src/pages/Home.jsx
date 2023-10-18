ddfaba0d1d3ce1beb4711e59ab2f2c7447aa0cf9
Jakub <jakubglinka9@gmail.com>
2023-10-18 20:04:44 +0200

    Improve quotes section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,15 +8,38 @@ function Home() {
       </h1>
       <img src="/src/img/bitcoin.png" alt="bitcoin" />
       <h2>
-        This is text. This is text. This is text. This is text. This is text
+        It&rsquo;s simple, effortless, fun and free!
+        {/* It&rsquo;s <span className="purple">simple</span>,{" "}
+        <span className="purple">effortless</span>,{" "}
+        <span className="purple">fun</span> and{" "}
+        <span className="purple">free!</span> */}
       </h2>
       <img src="/src/img/dollars.png" alt="dollars" />
       <br />
-      <div className="Home--quote">
-        <p>
-          &ldquo;It&apos;s not how much money you make, but how much money you keep, how
-          hard it works for you, and how many generations you keep it for&rdquo;— Robert Kiyosaki
-        </p>
+      <div className="Home--quotes-outer">
+        <div className="Home--quotes">
+          <div className="Home--quote">
+            <p>
+              &ldquo;It&apos;s not how much money you make, but how much money
+              you keep, how hard it works for you, and how many generations you
+              keep it for&rdquo;
+            </p>
+            <div className="Home--quote-author">Robert Kiyosaki</div>
+          </div>
+          
+          <div className="Home--quote">
+            <p>
+              &ldquo;Compound interest is the eighth wonder of the world; he who
+              understands it, earns it, he who doesn’t pays it&rdquo;
+            </p>
+            <div className="Home--quote-author">Albert Einstein</div>
+          </div>
+
+          <div className="Home--quote">
+            <p>&ldquo;The biggest risk of all, is not taking one&rdquo;</p>
+            <div className="Home--quote-author">Mellody Hobson</div>
+          </div>
+        </div>
       </div>
       <br />
       <ButtonLink to="/register" text="Start Investing" size="bg" />
