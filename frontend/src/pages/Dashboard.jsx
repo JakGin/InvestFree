102dda3a66c5682607bda5855993cef90b8f8013
Jakub <jakubglinka9@gmail.com>
2023-10-19 17:54:15 +0200

    Start Dashboard dev

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Stock from "/src/components/Stock";
+
+const url = `${import.meta.env.VITE_STOCK_API_URL}/AMZN`;
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_X_RapidAPI_Key,
+    "X-RapidAPI-Host": import.meta.env.VITE_X_RapidAPI_Host,
+  },
+};
 
 function Dashboard() {
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="Dashboard--container">
+      <div className="Dashboard--section">
+        <Stock />
+        <Stock />
+        <Stock />
+        <Stock />
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
