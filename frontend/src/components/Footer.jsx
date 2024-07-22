42d9d6303f39442dc4e5ac4d1ffdfb199f7ac8bc
Jakub Glinka <jakubglinka9@gmail.com>
2024-07-22 11:40:02 +0200

    Add Author to Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import { Link } from "@chakra-ui/react"
+import React from "react"
 
 function Footer() {
   return (
-    <div className="Footer--container">
+    <div className="Footer--container flex justify-between">
       <p>&copy; 2023 InvestFree. All Rights Reserved.</p>
+      <p>
+        Created by:{" "}
+        <Link href="https://github.com/JakGin" className="inline-block" isExternal>
+          <span className="whitespace-nowrap underline hover:text-green-500">Jakub Glinka</span>
+        </Link>
+      </p>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
