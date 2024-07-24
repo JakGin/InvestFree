386d39554d454b512c3c38b614e1991f767e989e
Jakub Glinka <jakubglinka9@gmail.com>
2024-07-24 10:51:03 +0200

    Change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,13 +61,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 # CORS_ALLOWED_ORIGINS = [
